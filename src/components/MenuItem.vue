a0ade5e95637a03e74e81bb22c93a431e12d6c5f
vadym-loik <loikfit@gmail.com>
2023-02-07 13:42:34 +0100

    add all files

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="menu-item">
+    <img class="menu-item__image" :src="image.source" :alt="image.alt" />
+    <div>
+      <h3>{{ name }}</h3>
+      <p>Price: {{ generatedPrice }} <span v-if="onSale">(10% off!)</span></p>
+      <p v-if="inStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      <div>
+        <label for="add-item-quantity">Quantity: {{ qty }}</label>
+        <input v-model.number="qty" id="add-item-quantity" type="number" />
+        <BaseButton @click="updateShoppingCart(qty)">
+          Add to shopping cart
+        </BaseButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import BaseButton from './BaseButton.vue';
+export default {
+  name: 'MenuItem',
+  components: {
+    BaseButton,
+  },
+  props: {
+    image: {
+      type: Object,
+      required: true,
+    },
+    inStock: {
+      type: Boolean,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      defaut: 1,
+    },
+  },
+  data() {
+    return {
+      onSale: false,
+    };
+  },
+  computed: {
+    generatedPrice() {
+      if (this.onSale) {
+        return (this.price * 0.9).toFixed(2);
+      } else {
+        return this.price;
+      }
+    },
+  },
+  methods: {
+    ...mapActions(['updateShoppingCart']),
+  },
+  beforeMount() {
+    const today = new Date().getDate();
+    if (today % 2 === 0) {
+      this.onSale = true;
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+.menu-item {
+  display: flex;
+  width: 500px;
+  justify-content: space-between;
+  margin-bottom: 30px;
+  &__image {
+    max-width: 300px;
+  }
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,61 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    restaurantName: 'Cafe with A Vue',
+    shoppingCart: 0,
+    simpleMenu: [
+      {
+        name: 'Crossiant',
+        image: {
+          source: '/images/crossiant.jpg',
+          alt: 'A crossiant',
+        },
+        inStock: true,
+        quantity: 1,
+        price: 2.99,
+      },
+      {
+        name: 'French Baguette',
+        image: {
+          source: '/images/french-baguette.jpeg',
+          alt: 'Four French Baguettes',
+        },
+        inStock: true,
+        quantity: 1,
+        price: 3.99,
+      },
+      {
+        name: 'Éclair',
+        image: {
+          source: '/images/eclair.jpg',
+          alt: 'Chocolate Éclair',
+        },
+        inStock: false,
+        quantity: 1,
+        price: 4.99,
+      },
+    ],
+  },
+  getters: {
+    copyright: (state) => {
+      const currentYear = new Date().getFullYear();
+
+      return `Copyright ${state.restaurantName} ${currentYear}`;
+    },
+  },
+  mutations: {
+    ADD_ITEMS_TO_SHOPPING_CART(state, amount) {
+      state.shoppingCart += amount;
+    },
+  },
+  actions: {
+    updateShoppingCart({ commit }, amount) {
+      commit('ADD_ITEMS_TO_SHOPPING_CART', amount);
+    },
+  },
+  modules: {},
+});
